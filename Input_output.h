02bd88fda28033d05640aed6ff7785735dd5e3f2
Chimichan-dorid <jacobymartin1@icloud.com>
2025-03-16 19:55:10 -0700

    Input_output class made. Building Menu and Print method.

--- Input_output.h
+++ Input_output.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <iostream>
+
 class Input_output
 {
+	std::string human_player, computer_player, row, column;
+	
+	Input_output();
+	void Menu();
+	void Print();
+
 };
 
--- main.cpp
+++ main.cpp
@@ -1,32 +1,14 @@
 #include <iostream>
 #include "gameBroad.h"
+#include "Input_output.h"
 
-gameBroad obj;
+gameBroad broad;
+Input_output inou;
 
 int main() {
-	std::string playerX;
-	std::string playerO;
-
-	std::string row;
-	std::string column;
-
-	std::cout << "Welcome to Tic-Tac-Toe!\n\n";
-	std::cout << "What's your name? : ";
-	std::cin >> playerX;
-	system("CLS");
-	std::cout << playerX;
-	std::cout << "\n\n ";
-
-	obj.display();
-
-	while (true) {
-		std::cout << "\nPick a row : ";
-		std::cin >> row;
-		std::cout << "Pick a column : ";
-		std::cin >> column;
-	}
-
-
+	
+	inou.Menu();
+	broad.display();
 
 }
 
